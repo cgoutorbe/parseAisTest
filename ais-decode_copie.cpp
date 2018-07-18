55d06e3985b98fb6688c6afd7f13a26f483f1281
Evann CLAVIER <evannclavier@MacBook-Pro-de-Evann.local>
2018-07-18 15:21:25 +0100

    maj decodeur

--- ais-decode_copie.cpp
+++ ais-decode_copie.cpp
@@ -111,11 +111,11 @@ int main(int argc, char **argv)
 
 	unsigned int x,st=0,fn=0;
 	//Initialisation ROS
-	ros::init(argc, argv, "talker");
+	ros::init(argc, argv, "ais_decodeur");
 	ros::NodeHandle n;
 
 	//Publisher & declaration message
-	ros::Publisher chat_lat = n.advertise<std_msgs::Flo64MultiArray>("Lat_Long_Bat", 1000);
+	ros::Publisher chat_lat = n.advertise<std_msgs::Float64MultiArray>("Lat_Long_Bat", 1000);
 	std_msgs::Float64MultiArray mes_latlong;
 
 	//Subscriber
@@ -133,57 +133,59 @@ int main(int argc, char **argv)
 		y = (x+1) * 6-5;
 		ais_binary.append(six_bit_table[z]);
 	}
-	//Decode: conversion binary to number (=> code ASCII)
-	string temp_s = ais_binary.substr(0,6);
-	int ais_message_type = bin_to_int(temp_s);
+	if (ais_binary > 137){
+		//Decode: conversion binary to number (=> code ASCII)
+		string temp_s = ais_binary.substr(0,6);
+		int ais_message_type = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(6,2);
-	int ais_repeat_indicator = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(6,2);
+		int ais_repeat_indicator = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(8,30);
-	int ais_mmsi = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(8,30);
+		int ais_mmsi = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(38,4);
-	int ais_navigation_status = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(38,4);
+		int ais_navigation_status = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(42,8);
-	int ais_rate_of_turn = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(42,8);
+		int ais_rate_of_turn = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(50,10);
-	int ais_speed_over_ground = bin_to_int(temp_s)/10;
+		temp_s = ais_binary.substr(50,10);
+		int ais_speed_over_ground = bin_to_int(temp_s)/10;
 
-	string ais_position_accuracy  = ais_binary.substr(60,1);
+		string ais_position_accuracy  = ais_binary.substr(60,1);
 
-	char west = ais_binary[61];
-	temp_s = ais_binary.substr(61,28);
-	bitset<28> hh(temp_s);
-	if (west == '1') {hh.flip();}
-	double ais_longitude = hh.to_ulong();;
-	ais_longitude = ais_longitude/10000/60;
-	if (west == '1') {ais_longitude *= -1;}
+		char west = ais_binary[61];
+		temp_s = ais_binary.substr(61,28);
+		bitset<28> hh(temp_s);
+		if (west == '1') {hh.flip();}
+		double ais_longitude = hh.to_ulong();;
+		ais_longitude = ais_longitude/10000/60;
+		if (west == '1') {ais_longitude *= -1;}
 
-	char south = ais_binary[89];
-	temp_s = ais_binary.substr(89,27);
-	bitset<27> ii(temp_s);
-	if (south == '1') {ii.flip();}
-	double ais_latitude = ii.to_ulong();
-	ais_latitude = ais_latitude/10000/60;
-	if (south == '1') {ais_latitude *= -1;}
+		char south = ais_binary[89];
+		temp_s = ais_binary.substr(89,27);
+		bitset<27> ii(temp_s);
+		if (south == '1') {ii.flip();}
+		double ais_latitude = ii.to_ulong();
+		ais_latitude = ais_latitude/10000/60;
+		if (south == '1') {ais_latitude *= -1;}
 
-	temp_s = ais_binary.substr(116,12);
-	int ais_course_over_ground = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(116,12);
+		int ais_course_over_ground = bin_to_int(temp_s);
 
-	temp_s = ais_binary.substr(128,9);
-	int ais_true_heading = bin_to_int(temp_s);
+		temp_s = ais_binary.substr(128,9);
+		int ais_true_heading = bin_to_int(temp_s);
 
-	//Fill a message objet to keep it somewhere
-	Message message;
-	message.setAll( id, ais_message_type, ais_repeat_indicator, ais_mmsi, ais_navigation_status, ais_rate_of_turn, ais_speed_over_ground, ais_position_accuracy, ais_longitude, ais_latitude, ais_course_over_ground, ais_true_heading);
-	mes_latlong.data[0] = message.getLatitude();
-	mes_latlong.data[1] = message.getLongitude();
+		//Fill a message objet to keep it somewhere
+		Message message;
+		message.setAll( id, ais_message_type, ais_repeat_indicator, ais_mmsi, ais_navigation_status, ais_rate_of_turn, ais_speed_over_ground, ais_position_accuracy, ais_longitude, ais_latitude, ais_course_over_ground, ais_true_heading);
+		mes_latlong.data[0] = message.getLatitude();
+		mes_latlong.data[1] = message.getLongitude();
 
-	//Send the message
-	chat_lat.publish(mes_latlong);
+		//Send the message
+		chat_lat.publish(mes_latlong);
+	}
 
 	ros::spinOnce();
 	loop_rate.sleep();
